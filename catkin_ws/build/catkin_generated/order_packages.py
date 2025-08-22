95a6282f7edb3632a91e5fb24ea90cca5530ab14
Parham Fatemi <pfatemi7@gmail.com>
2025-08-22 13:00:19 -0700

    Remove build files from Git tracking and update .gitignore

--- catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/skyscouter/catkin_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/skyscouter/catkin_ws/src/unilidar_sdk/unitree_lidar_ros/devel;/home/skyscouter/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/skyscouter/catkin_ws/src/unilidar_sdk/unitree_lidar_ros/devel;/home/skyscouter/catkin_ws/devel;/opt/ros/melodic' != '' else []
